b1c1eba5ef5139696f243463e3475a138196750a
Nathan BOURRY <nathan.bourry@gmail.com>
2023-08-31 20:58:33 +0200

    Fisrt version

--- script/menu.js
+++ script/menu.js
@@ -1,11 +1,26 @@
-const menuItems = document.querySelectorAll('.container .menu li'); 
-
-menuItems[2].addEventListener('mouseenter', () => {
-    menuItems[0].style.transform = 'translateX(0)';
-    menuItems[1].style.transform = 'translateX(0)';
+const menuItems = document.querySelectorAll('.container .menu li span');
+const menu = document.querySelector('.menu');
+menuItems.forEach((item, index) => {
+            
+    item.addEventListener('mouseenter', () => {
+        menuItems.forEach((item, index) => {
+            
+            if (index !== null) {
+                switch (index) {
+                    case 0:
+                        item.style.transform = `translateX(-20vw)`;
+                        break;
+                    case 1:
+                        item.style.transform = `translateX(-10vw)`;
+                        break;
+                }
+            }
+        });
+    });
 });
 
-menuItems[2].addEventListener('mouseleave', () => {
-    menuItems[0].style.transform = 'translateX(20vw)';
-    menuItems[1].style.transform = 'translateX(10vw)';
+menu.addEventListener('mouseleave', () => {
+    menuItems.forEach((item, index) => {
+        item.style.transform = `translateX(0)`;
+    });
 });
